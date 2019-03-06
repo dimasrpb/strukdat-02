c6ce836b240b4b5c4ff02694e689cd21d2a7b5d1
dimasrpb <48269278+dimasrpb@users.noreply.github.com>
2019-03-06 10:39:23 +0700

    tugas pretest 2 strukdat

---
+++ pretest2 strukdat.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+	
+void movezeroToFront (int arr[], int n){}
+void input(int (&arr)[100], int& n){}
+void output(int arr[], int n){}
+
+int main (){
+	int arr[100];
+	int n;
+	input(arr,n);
+	moveZeroToFront(arr,n);
+	output(arr, n);
+}
